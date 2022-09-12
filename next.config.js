c87395112a338c66355ef335542a620482a6b84c
Rahul Meka <rahul.meka@pens.com>
2022-09-13 03:11:37 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    // assuming you were using the Sanity.io image CDN
+    // domains is an array of comma-separated strings
+    // ['cdn.sanity.io', 'cdn.not-sanity.io', 'another domain']
+    domains: ['www.gstatic.com', 'live.staticflickr.com'],
+  }
+
 }
 
 module.exports = nextConfig
---
+++ pages/gallery.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function Gallery(props) {
+
+
+    return (
+        <>
+            <Image src="https://live.staticflickr.com/65535/52332920296_fe8fca84b4_h.jpg" width={500} height={500} />
+            <Image src="https://www.gstatic.com/webp/gallery/2.jpg" alt="Picture of the author"
+                width={500}
+                height={500} />
+            <Image src="https://www.gstatic.com/webp/gallery/3.jpg" alt="Picture of the author"
+                width={500}
+                height={500} />
+            <Image src="https://www.gstatic.com/webp/gallery/4.jpg" alt="Picture of the author"
+                width={500}
+                height={500} />
+
+            <Image src="https://www.gstatic.com/webp/gallery/2.jpg" alt="Picture of the author"
+                width={500}
+                height={500} />
+            <Image src="https://www.gstatic.com/webp/gallery/3.jpg" alt="Picture of the author"
+                width={500}
+                height={500} />
+            <Image src="https://www.gstatic.com/webp/gallery/4.jpg" alt="Picture of the author"
+                width={500}
+                height={500} />
+
+            <Image src="https://www.gstatic.com/webp/gallery/2.jpg" alt="Picture of the author"
+                width={500}
+                height={500} />
+            <Image src="https://www.gstatic.com/webp/gallery/3.jpg" alt="Picture of the author"
+                width={500}
+                height={500} />
+            <Image src="https://www.gstatic.com/webp/gallery/4.jpg" alt="Picture of the author"
+                width={500}
+                height={500} />
+
+            <Image src="https://www.gstatic.com/webp/gallery/2.jpg" alt="Picture of the author"
+                width={500}
+                height={500} />
+            <Image src="https://www.gstatic.com/webp/gallery/3.jpg" alt="Picture of the author"
+                width={500}
+                height={500} />
+            <Image src="https://www.gstatic.com/webp/gallery/4.jpg" alt="Picture of the author"
+                width={500}
+                height={500} />
+
+            <Image src="https://www.gstatic.com/webp/gallery/2.jpg" alt="Picture of the author"
+                width={500}
+                height={500} />
+            <Image src="https://www.gstatic.com/webp/gallery/3.jpg" alt="Picture of the author"
+                width={500}
+                height={500} />
+            <Image src="https://www.gstatic.com/webp/gallery/4.jpg" alt="Picture of the author"
+                width={500}
+                height={500} />
+
+        </>
+    )
+}
