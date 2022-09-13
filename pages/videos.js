90db8656611445d5e4eda6453e7ea5b39900e4cf
Rahul Meka <rahul.meka@pens.com>
2022-09-13 22:13:23 +0530

    pwa

--- pages/videos.js
+++ pages/videos.js
@@ -10,26 +10,7 @@ export default function Videos(props) {
                     <VideoPlayer src="https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" />
                 </div>
             </div>
-            <div className='videos'>
-                <div className="grid">
-                    <VideoPlayer src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4" />
-                </div>
-            </div>
-            <div className='videos'>
-                <div className="grid">
-                    <VideoPlayer src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4" />
-                </div>
-            </div>
-            <div className='videos'>
-                <div className="grid">
-                    <VideoPlayer src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4" />
-                </div>
-            </div>
-            <div className='videos'>
-                <div className="grid">
-                    <VideoPlayer src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4" />
-                </div>
-            </div>
+
         </>
     )
 }
